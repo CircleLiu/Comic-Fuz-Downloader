da18dcfba8739b3ed085df2df2c939e4845e1415
CircleLiu <1176092962@qq.com>
2022-12-10 19:55:51 +0800

    update version

--- comic-fuz-downloader.user.js
+++ comic-fuz-downloader.user.js
@@ -2,7 +2,7 @@
 // @name              Comic Fuz Downloader
 // @name:en           Comic Fuz Downloader
 // @namespace         http://circleliu.cn
-// @version           0.4.4
+// @version           0.4.5
 // @description       Userscript for download comics on Comic Fuz
 // @description:en    Userscript for download comics on Comic Fuz
 // @author            Circle
