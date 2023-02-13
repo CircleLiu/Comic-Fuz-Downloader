cccb21e3587ee35694d109a5bd8e9e10ad6766a1
2Loong6 <1727963726@qq.com>
2023-02-13 12:03:22 +0800

    fix: add {}

--- comic-fuz-downloader.user.js
+++ comic-fuz-downloader.user.js
@@ -321,8 +321,9 @@
       if (metadata.bookIssue) {
         return metadata.bookIssue.bookIssueName.trim()
       } else if (metadata.viewerTitle) {
-        if (!!metadata.sns && !!metadata.sns.body)
-          return  metadata.sns.body.match(/(?<=「).*(?=」)/).trim()
+        if (!!metadata.sns && !!metadata.sns.body) {
+          return metadata.sns.body.match(/(?<=「).*(?=」)/).trim()
+        }
         return metadata.viewerTitle.trim()
       } else if (metadata.magazineIssue) {
         return metadata.magazineIssue.magazineName.trim() + ' ' + metadata.magazineIssue.magazineIssueName.trim()
